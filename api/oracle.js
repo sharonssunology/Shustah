cea5c6b62f58367aa3f3aaae51de068b99fcf0e2
sharonssunology <31543959+sharonssunology@users.noreply.github.com>
2025-12-20 19:32:34 -0500

    Update oracle.js

--- api/oracle.js
+++ api/oracle.js
@@ -1,22 +1,22 @@
 // api/oracle.js (CommonJS)
 
-const SHUS_CARDS = require("../data/shus-cards.js");
-const SHUS_TEXTS = require("../data/shus-texts.js");
+const SHUSTAH-CARDS = require("../data/shustah-cards.js");
+const SHUSTAH-TEXTS = require("../data/shustah-texts.js");
 
 module.exports = (req, res) => {
   try {
-    const degree = Math.floor(Math.random() * 360) + 1;
+    const card = Math.floor(Math.random() * 70) + 1;
 
-    const symbol = SHUS_CARDS[card];
-    const text = SHUS_TEXTS[card];
+    const symbol = SHUSTAH_CARDS[card];
+    const text = SHUSTAH_TEXTS[card];
 
     if (!symbol || !text) {
       return res.status(500).json({
-        error: `Missing data for card ${card}`
+        error: `Missing oracle data for card ${card}`
       });
     }
 
-    const imageUrl = `/shustah-images/${degree}.jpg`;
+    const imageUrl = `/symbol-images/${card}.jpg`;
 
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET");
